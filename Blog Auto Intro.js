87bdefc342042cc0ff853ba5c5e585d41e2d3e08
shakib04 <51458806+shakib04@users.noreply.github.com>
2020-07-17 00:52:49 +0600

    Update Blog Auto Intro.js

--- Blog Auto Intro.js
+++ Blog Auto Intro.js
@@ -1,3 +1,4 @@
- var postTitle = $(".entry-title").text();
+ 
+var postTitle = $("title").text();
         var introText = "Hello Mr./ Mrs today we will learn and develop '" + postTitle + "' Source code and demo is available. I keep simple to understand. If you find a problem comment here. ";
         $(".post-body").prepend(introText + "<br/> <br/>");
